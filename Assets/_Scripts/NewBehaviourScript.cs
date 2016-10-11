cd3485931ea1fc30f8a29de9af8864aa97f2cad5
LucasMiddlekauff <20lmiddlekauff@cr.k12.ia.us>
2016-10-11 09:11:43 -0500

    Initail save

---
+++ Assets/_Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public CharacterController MyController;
+	public float speed = 3f;
+	// Use this for initialization
+	void Update () 
+	{
+		
+		float x = 3f;
+		float y = 2f;
+		float z = 1f;
+		Vector3 MyVector = new Vector3 (0, 0, 0); 
+		MyVector.x = Input.GetAxis ("Horizontal");
+		MyVector.z = Input.GetAxis ("Vertical");
+		MyVector += speed;
+
+		MyController.SimpleMove (MyVector);
+	}
+	
+
+}
